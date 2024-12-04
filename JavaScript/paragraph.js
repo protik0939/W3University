af68947a7a6d635b889f95a70c1a047550cafb0d
Rafi <ishmakrahat02@gmail.com>
2024-12-04 19:17:29 +0600

    paragraph ordek up

---
+++ JavaScript/paragraph.js
@@ -0,0 +1,33 @@
+const questions = [
+    {
+      question: "Which HTML tag is used to define a paragraph?",
+      options: ["<p>", "<div>", "<span>", "<h1>"],
+      correct: "<p>",
+    },
+    {
+      question: "How can you add extra space between paragraphs in HTML?",
+      options: [
+        "Using the <br> tag",
+        "Using the CSS margin property",
+        "Using the <p> tag repeatedly",
+        "Using the <h1> tag"
+      ],
+      correct: "Using the CSS margin property",
+    },
+    {
+      question: "What is the default alignment of a paragraph in HTML?",
+      options: [ "Center", "Right", "Left", "Justify"],
+      correct: "Left",
+    },
+    {
+      question: "Which of the following CSS property is used to style the line spacing of a paragraph?",
+      options: ["line-height", "letter-spacing", "word-spacing", "text-align"],
+      correct: "line-height",
+    },
+    {
+      question: "How do you make the text in a paragraph italic in HTML?",
+      options: ["<p style='italic'>", "<i>", "<italic>", "<em>"],
+      correct: "<em>",
+    },
+];
+
